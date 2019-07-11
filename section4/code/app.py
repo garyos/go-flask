2f3b0019a3c833acba168c3b74661d23ca5b145b
garyos <g.osman89@gmail.com>
2019-07-11 02:25:29 +0100

    added parser

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -40,7 +40,15 @@ class Item(Resource):
         return {'message' : "Item: '{}' deleted".format(name)}, 200
 
     def put(self, name):
-        data = request.get_json()
+        parser=reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help='This field cannot be left blank!'
+        )
+        data = parser.parse_args()
+
+        #data = request.get_json()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name':name, 'price':data['price']}
